74ac6f3d0ab7a767065dbc1a9230bad93b065540
Luke Chandler <lhchandler4@gmail.com>
2019-05-07 11:23:13 -0700

    modifications to app.js and README

--- src/App.js
+++ src/App.js
@@ -16,24 +16,31 @@ class Counter extends React.Component {
 
     this.state = {
       count: 0,
+      clicks: 0,
     }
   }
 
+
   handlePlus = e => {
     e.preventDefault();
+    let clicks = this.state.clicks + 1;
     let count = this.state.count + 1;
     this.setState({ count });
+    this.setState({ clicks });
   }
 
   handleMinus = e => {
     e.preventDefault();
+    let clicks = this.state.clicks + 1;
     let count = this.state.count - 1;
     this.setState({ count });
+    this.setState({ clicks });
   }
 
   render() {
     return (
       <div>
+        <p>Number Of Clicks: {this.state.clicks}</p>
         <p>{this.state.count}</p>
         <button onClick={this.handleMinus}>-</button>
         <button onClick={this.handlePlus}>+</button>
